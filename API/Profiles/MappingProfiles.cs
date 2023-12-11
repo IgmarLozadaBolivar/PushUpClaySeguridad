748f9688559c102b0df27dba110f76d9ae4a4e47
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-12-11 18:17:43 -0500

    feat: :sparkles: Entity and DTOS Mapping

--- API/Profiles/MappingProfiles.cs
+++ API/Profiles/MappingProfiles.cs
@@ -9,5 +9,20 @@ public class MappingProfiles : Profile
     {
         CreateMap<Rol, RolDto>().ReverseMap();
         CreateMap<User, UserDto>().ReverseMap();
+
+        CreateMap<Pais, PaisDto>().ReverseMap();
+        CreateMap<Departamento, DepartamentoDto>().ReverseMap();
+        CreateMap<Ciudad, CiudadDto>().ReverseMap();
+        CreateMap<Persona, PersonaDto>().ReverseMap();
+        CreateMap<TipoContacto, TipoContactoDto>().ReverseMap();
+        CreateMap<TipoDireccion, TipoDireccionDto>().ReverseMap();
+        CreateMap<TipoPersona, TipoPersonaDto>().ReverseMap();
+        CreateMap<Turno, TurnoDto>().ReverseMap();
+        CreateMap<Programacion, ProgramacionDto>().ReverseMap();
+        CreateMap<ContactoPer, ContactoPerDto>().ReverseMap();
+        CreateMap<Contrato, ContratoDto>().ReverseMap();
+        CreateMap<DirPersona, DirPersonaDto>().ReverseMap();
+        CreateMap<Estado, EstadoDto>().ReverseMap();
+        CreateMap<CategoriaPer, CategoriaPerDto>().ReverseMap();
     }
 }
