a2f959a369926a5108ce5e760a87d7247e4b1538
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-12-11 17:39:29 -0500

    feat: :sparkles: News adds interfaces of respective entities remaining in the UnitOfWork

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -5,5 +5,20 @@ public interface IUnitOfWork
     IUser Users { get; }
     IRol Roles { get; }
 
+    IPais Paises { get; }
+    IDepartamento Departamentos { get; }
+    ICiudad Ciudades { get; }
+    IPersona Personas { get; }
+    ITipoContacto TipoContactos { get; }
+    ITipoDireccion TipoDirecciones { get; }
+    ITipoPersona TipoPersonas { get; }
+    ICategoriaPer CategoriaPers { get; }
+    IContrato Contratos { get; }
+    IContratoPer GetContratoPers { get; }
+    ITurno Turnos { get; }
+    IEstado Estados { get; }
+    IDirPersona DirPersonas { get; }
+    IProgramacion Programacions { get; }
+
     Task<int> SaveAsync();
 }
