90d27c3cc6c7045eb6ef1f6abeaa9ea384877324
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-12-11 17:24:24 -0500

    feat: :sparkles: New changes and new adds features

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
+using API.Extensions;
+using API.Helpers;
+using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Persistence.Data;
 using Serilog;
 
@@ -12,10 +17,55 @@ var logger = new LoggerConfiguration()
 builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
+builder.Services.AddAplicacionServices();
+builder.Services.ConfigureRatelimiting();
+builder.Services.ConfigureApiVersioning();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Clay Security", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.ConfigureCors();
+
+builder.Services.AddDbContext<DbAppContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+builder.Services.AddJwt(builder.Configuration);
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Garden's API v1");
+        }
+    );
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -31,6 +81,8 @@ using (var scope = app.Services.CreateScope())
     {
         var context = services.GetRequiredService<DbAppContext>();
         await context.Database.MigrateAsync();
+        await DbAppContextSeed.SeedRolesAsync(context, loggerFactory);
+        await DbAppContextSeed.SeedAsync(context, loggerFactory);
     }
     catch (Exception ex)
     {
@@ -39,7 +91,13 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
+
+app.UseIpRateLimiting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
